1d58de0b73d6f47c168a76896f6adae70bd59227
dienyz <dienyy9@gmail.com>
2022-11-11 23:56:29 +0700

    1

--- app/src/main/java/id/putraprima/skorbola/MainActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MainActivity.java
@@ -1,15 +1,118 @@
 package id.putraprima.skorbola;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    private EditText ronde, namatim1, namatim2;
+    private ImageView logotim1;
+    private ImageView logotim2;
+    private Button btnnext;
+    private Uri uritim1, uritim2;
+
+    private static final String TAG = MainActivity.class.getCanonicalName();
+    private static final int TEAM1_REQUEST_CODE = 1;
+    private static final int TEAM2_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ronde=findViewById(R.id.rondke);
+        namatim1=findViewById(R.id.team1);
+        namatim2=findViewById(R.id.team2);
+        logotim1=findViewById(R.id.team1_logo);
+        logotim2=findViewById(R.id.team2_logo);
+        btnnext=findViewById(R.id.btn_team);
+
+        logotim1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI), TEAM1_REQUEST_CODE);
+            }
+        });
+
+        logotim2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI), TEAM2_REQUEST_CODE);
+            }
+        });
+
+        btnnext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sronde = ronde.getText().toString();
+                String snamatim1 = namatim1.getText().toString();
+                String snamatim2 = namatim2.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, MatchActivity.class);
+                intent.putExtra("rondee", sronde);
+                intent.putExtra("namatim1", snamatim1);
+                intent.putExtra("namatim2", snamatim2);
+                intent.putExtra("tim1logo", uritim1.toString());
+                intent.putExtra("tim2logo", uritim2.toString());
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_CANCELED){
+            Log.d(TAG, "Pilih gambar dicancel");
+            return;
+        }
+        else if(requestCode == TEAM1_REQUEST_CODE){
+            if(data != null){
+                try {
+                    uritim1 = data.getData();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uritim1);
+                    logotim1.setImageBitmap(bitmap);
+                }
+                catch (IOException error){
+                    Toast.makeText(this, "Can't load image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, error.getMessage());
+                }
+            }
+        }
+        else if(requestCode == TEAM2_REQUEST_CODE){
+            if(data != null){
+                try {
+                    uritim2 = data.getData();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uritim2);
+                    logotim2.setImageBitmap(bitmap);
+                }
+                catch (IOException error){
+                    Toast.makeText(this, "Can't load image", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, error.getMessage());
+                }
+            }
+        }
+    }
+}
+
+
+
         //TODO
         //Fitur Main Activity
         //1. Validasi Input Home Team
@@ -17,5 +120,3 @@ public class MainActivity extends AppCompatActivity {
         //3. Ganti Logo Home Team
         //4. Ganti Logo Away Team
         //5. Next Button Pindah Ke MatchActivity
-    }
-}
--- app/src/main/java/id/putraprima/skorbola/MatchActivity.java
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -2,17 +2,155 @@ package id.putraprima.skorbola;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MatchActivity extends AppCompatActivity {
+    private TextView tvronde, tvnamatim1, tvnamatim2, skortim1, skortim2;
+    private int skor1, skor2;
+    private ImageView ivlogotim1, ivlogotim2;
+    private Button btncek, add1tim1, add2tim1, add3tim1, resettim1, add1tim2, add2tim2, add3tim2, resettim2;
+    private String winner, snamatim1, snamatim2, sarond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+
+        tvronde = findViewById(R.id.rondke);
+        tvnamatim1 = findViewById(R.id.nmtv1);
+        tvnamatim2 = findViewById(R.id.nmtv2);
+        skortim1 = findViewById(R.id.skorteam1);
+        skortim2 = findViewById(R.id.skorteam2);
+        ivlogotim1 = findViewById(R.id.team1_logo);
+        ivlogotim2 = findViewById(R.id.team2_logo);
+        btncek = findViewById(R.id.btn_cek);
+
+        add1tim1 = findViewById(R.id.tambah1);
+        add2tim1 = findViewById(R.id.tambah2);
+        add3tim1 = findViewById(R.id.tambah3);
+        resettim1 = findViewById(R.id.reset);
+
+        add1tim2 = findViewById(R.id.tambah1_2);
+        add2tim2 = findViewById(R.id.tambah2_2);
+        add3tim2 = findViewById(R.id.tambah3_2);
+        resettim2 = findViewById(R.id.reset2);
+
+        skor1 = 0;
+        skor2 = 0;
+        skortim1.setText(String.valueOf(skor1));
+        skortim2.setText(String.valueOf(skor2));
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            if (bundle.getString("tim1logo")!=null && bundle.getString("tim2logo")!=null ){
+                ivlogotim1.setImageURI(Uri.parse(bundle.getString("tim1logo")));
+                ivlogotim2.setImageURI(Uri.parse(bundle.getString("tim2logo")));
+            }
+            sarond = bundle.getString("rondee");
+            snamatim1 = bundle.getString("namatim1");
+            snamatim2 = bundle.getString("namatim2");
+            tvronde.setText(sarond);
+            tvnamatim1.setText(snamatim1);
+            tvnamatim2.setText(snamatim2);
+        }else {
+            Toast.makeText(MatchActivity.this, "Intent is missing", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        add1tim1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                skor1 += 1;
+                skortim1.setText(String.valueOf(skor1));
+            }
+        });
+        add2tim1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                skor1 += 2;
+                skortim1.setText(String.valueOf(skor1));
+            }
+        });
+        add3tim1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                skor1 += 3;
+                skortim1.setText(String.valueOf(skor1));
+            }
+        });
+        resettim1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                skor1 = 0;
+                skortim1.setText(String.valueOf(skor1));
+            }
+        });
+
+        //---------------------
+
+        add1tim2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                skor2 += 1;
+                skortim2.setText(String.valueOf(skor2));
+            }
+        });
+        add2tim2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                skor2 += 2;
+                skortim2.setText(String.valueOf(skor2));
+            }
+        });
+        add3tim2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                skor2 += 3;
+                skortim2.setText(String.valueOf(skor2));
+            }
+        });
+        resettim2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                skor2 = 0;
+                skortim2.setText(String.valueOf(skor2));
+            }
+        });
+
+        //---------------------------
+
+        btncek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                winner = "empty";
+                if(skor1 > skor2){
+                    winner = snamatim1;
+                }
+                else if (skor1 == skor2){
+                    winner = "Seri";
+                }
+                else {
+                    winner = snamatim2;
+                }
+
+                Intent intent = new Intent(MatchActivity.this, ResultActivity.class);
+                intent.putExtra("winner", winner);
+                intent.putExtra("rondee", sarond);
+                startActivity(intent);
+            }
+        });
+    }
+}
+
         //TODO
         //1.Menampilkan detail match sesuai data dari main activity
         //2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
         //3.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang ke ResultActivity, jika seri di kirim text "Draw"
-    }
-}
